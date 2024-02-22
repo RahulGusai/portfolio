7f16ec5ec13373603bd6bc5d13814b9145494e96
Rahul Gusai <rgusai97@gmail.com>
2024-02-23 01:18:01 +0530

    Added new cmds

--- src/CommandProcessor.js
+++ src/CommandProcessor.js
@@ -5,14 +5,14 @@ class CommandProcessor {
     this.terminalInputElem = terminalInputElem;
     this.commands = {
       default: {
-        description: '',
+        description: 'Default command when the user enters an invalid command',
         outputType: 'row',
         handler: function (arg, dirsNavigated) {
           return config.default_cmd_output_list;
         },
       },
       ls: {
-        description: '',
+        description: 'Lists all the files in the present directory',
         outputType: 'row',
         handler: function (arg, dirsNavigated) {
           const directory =
@@ -26,7 +26,7 @@ class CommandProcessor {
       },
 
       cd: {
-        description: '',
+        description: 'Change the current directory',
         outputType: 'row',
         handler: function (arg, dirsNavigated) {
           if (!arg || arg in config.system_dirs || arg === '..') {
@@ -36,7 +36,45 @@ class CommandProcessor {
           return [{ value: `cd: No such file or directory: ${arg}` }];
         },
       },
+
+      pwd: {
+        description: 'Print the path to the current working directory.',
+        outputType: 'row',
+        handler: function (aeg, dirsNavigated) {
+          const pwdStr =
+            dirsNavigated.length === 0
+              ? '/home'
+              : `/home/${dirsNavigated.join('/')}`;
+          return [{ value: pwdStr }];
+        },
+      },
+
+      help: {
+        description: '',
+        outputType: 'column',
+        handler: null,
+      },
     };
+    this.commands.help.handler = this.createHelpCmdOutput.bind(this);
+  }
+
+  createHelpCmdOutput() {
+    const data = [
+      {
+        value:
+          'You can run the following commands to play around this terminal and know more about me. :)',
+      },
+    ];
+
+    const cmdsDescription = [];
+    Object.entries(this.commands)
+      .filter(([key]) => key !== 'default' && key !== 'help')
+      .forEach(([key, value]) => {
+        const { description } = value;
+        cmdsDescription.push({ value: key }, { value: `-- ${description}` });
+      });
+
+    return [...data, ...cmdsDescription];
   }
 
   getCommandObject(commandName) {
